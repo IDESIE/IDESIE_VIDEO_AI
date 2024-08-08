8dcf4ceeeaebed8d072ceb75892350aafde652b5
idesiegramos <159062617+idesiegramos@users.noreply.github.com>
2024-08-08 20:56:21 +0200

    Delete local_PC_version directory

--- local_PC_version/repositorio.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Repositorio de clases")
-st.write("Repositorio con los vídeos de las clases")
-st.write("Lorem ipsum odor amet, consectetuer adipiscing elit. Elementum dignissim neque suscipit nec montes mus ridiculus libero.")
